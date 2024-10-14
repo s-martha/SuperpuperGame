d737d12152b0a3e0d4ecc0e7664f58bddc47567f
mars <martha.stepashkina@yandex.ru>
2024-10-14 11:26:07 +0300

    Constants.java added from somewhere

---
+++ src/Constants.java
@@ -0,0 +1,9 @@
+public class Constants {
+    public static int TimerDelay = 150;
+    public static int WindowW = 1500;
+    public static int WindowH = 900;
+    public static int SnakePartW = 50;
+    public static int SnakePartH = 50;
+    public static int FoodW = 25;
+    public static int FoodH = 25;
+}
