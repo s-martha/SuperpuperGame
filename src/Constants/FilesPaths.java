c2bdcbe3813cb612f4f626b68e2f26dc2a765061
mars <martha.stepashkina@yandex.ru>
2024-10-14 13:01:53 +0300

    background image added

--- src/Constants/FilesPaths.java
+++ src/Constants/FilesPaths.java
@@ -3,5 +3,5 @@ package Constants;
 public class FilesPaths {
     public static String Ship1 = "src/Files/ship1.png";  // стрелками
     public static String Ship2 = "src/Files/ship2.png";  // wasd
-    public static String BackgroundImage = "";
+    public static String BackgroundImage = "src/Files/background.jpg";
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -12,6 +12,7 @@ public class GamePanel extends JPanel implements ActionListener {
     public Ship ship1;  // arrows
     public Ship ship2;  // wasd
     Timer timer;
+    public Image BackgroundImage;
 
     public GamePanel() throws IOException {
         setFocusable(true);
@@ -22,7 +23,8 @@ public class GamePanel extends JPanel implements ActionListener {
             Image imageShip2 = ImageIO.read(new File(FilesPaths.Ship2));
             this.ship1 = new Ship(Constants.Ship1SpawnX, Constants.Ship1SpawnY, Constants.Ship1Speed, Constants.StartAngleShip1, imageShip1, null, 1);
             this.ship2 = new Ship(Constants.Ship2SpawnX, Constants.Ship2SpawnY, Constants.Ship2Speed, Constants.StartAngleShip2, imageShip2, null, 1);
-        }
+
+            this.BackgroundImage = ImageIO.read(new File(FilesPaths.BackgroundImage));}
         catch (IOException e) {
             System.out.println("Картинка не загрузилась(( облом");
             throw new RuntimeException(e);
@@ -32,6 +34,7 @@ public class GamePanel extends JPanel implements ActionListener {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        g.drawImage(BackgroundImage, 0, 0, null);
         ship1.draw(g);
         ship2.draw(g);
     }
